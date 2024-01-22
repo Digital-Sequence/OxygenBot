257f9e82b0d6d447416d835c5b7b2381237b1b8a
Dmytro Schevchenko <jammer__@mailfence.com>
2024-01-22 09:05:45 +0200

    Altered some tables and added new ones

--- mysql-schema/mysql-schema.sql
+++ mysql-schema/mysql-schema.sql
@@ -12,7 +12,6 @@ USE `bot`;
 DROP TABLE IF EXISTS `BANS`;
 CREATE TABLE `BANS` (
   `rowid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `IS_BAN` bit(1) DEFAULT NULL,
   `GUILD_ID` bigint(20) unsigned NOT NULL,
   `USER_ID` bigint(20) unsigned NOT NULL,
   `USERNAME` text DEFAULT NULL,
@@ -44,13 +43,24 @@ CREATE TABLE `MESSAGES` (
 DROP TABLE IF EXISTS `MUTES`;
 CREATE TABLE `MUTES` (
   `rowid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `IS_MUTE` bit(1) DEFAULT NULL,
   `GUILD_ID` bigint(20) unsigned NOT NULL,
   `USER_ID` bigint(20) unsigned NOT NULL,
   `EXPIRES` bigint(20) unsigned DEFAULT NULL,
   `DATE` bigint(20) unsigned NOT NULL,
   `CANCELLED` bit(1) DEFAULT NULL,
   PRIMARY KEY (`rowid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `PREFERENCES`
+--
+
+DROP TABLE IF EXISTS `PREFERENCES`;
+CREATE TABLE `PREFERENCES` (
+  `GUILD_ID` bigint(20) unsigned NOT NULL,
+  `MAX_WARNS` tinyint(4) unsigned NOT NULL,
+  `WARN_PUNISHMENT` text NOT NULL,
+  PRIMARY KEY (`MAX_WARNS`,`GUILD_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -67,6 +77,32 @@ CREATE TABLE `ROLES` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Table structure for table `UNBANS`
+--
+
+DROP TABLE IF EXISTS `UNBANS`;
+CREATE TABLE `UNBANS` (
+  `rowid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `GUILD_ID` bigint(20) unsigned NOT NULL,
+  `USER_ID` bigint(20) unsigned NOT NULL,
+  `DATE` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`rowid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `UNMUTES`
+--
+
+DROP TABLE IF EXISTS `UNMUTES`;
+CREATE TABLE `UNMUTES` (
+  `rowid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `GUILD_ID` bigint(20) unsigned NOT NULL,
+  `USER_ID` bigint(20) unsigned NOT NULL,
+  `DATE` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`rowid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
 -- Table structure for table `WARNS`
 --
 
@@ -75,7 +111,7 @@ CREATE TABLE `WARNS` (
   `rowid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `GUILD_ID` bigint(20) unsigned NOT NULL,
   `USER_ID` bigint(20) unsigned NOT NULL,
-  `COUNT` int(10) unsigned NOT NULL,
+  `COUNT` tinyint(3) unsigned NOT NULL,
   `EXPIRES` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`rowid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
